2974bd01c7ea1177f148772c18a73f7feb5b9141
lccardenasma <laurac1996@outlook.es>
2025-07-24 11:10:13 -0500

    corrige PORT

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const usersRoutes = require('./users'); // ✅ Asegúrate de que el archivo user
 const cors = require('cors');
 const { initDB } = require('./db');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(cors());
 app.use(express.json());
